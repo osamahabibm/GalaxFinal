e4e64a19d08d661472916c828986acc6cf6ddd8d
osamahabibm <40914381+osamahabibm@users.noreply.github.com>
2021-12-29 04:16:28 -0800

    Add files via upload

---
+++ SkyWebJs/autoComplete.js
@@ -0,0 +1,14 @@
+
+ 
+	$(function(){
+		
+			$('.searchh').focusin(function(){
+				$('.auto_complete2').fadeIn();	
+			});
+			$('.searchh').focusout(function(){
+				$('.auto_complete2').fadeOut();	
+			});
+
+	});
+
+
